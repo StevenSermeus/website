9333f31934af787fb87dcd9ae8145325b62f32e6
Sermeus Steven <etu44050@henallux.be>
2024-04-15 07:28:27 +0200

    Update about section

---
+++ src/components/GithubRepo.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { Star, GitFork } from "lucide-react";
+interface Repo {
+  name: string;
+  description: string;
+  html_url: string;
+  language: string;
+  forks: number;
+  stargazers_count: number;
+}
+
+function GithubRepo() {
+  const [repos, setRepos] = React.useState<Repo[]>([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users/StevenSermeus/repos?sort=pushed")
+      .then(response => response.json())
+      .then(
+        result => {
+          setRepos(result.slice(0, 3));
+        },
+        error => {
+          console.error(error);
+        }
+      );
+  }, []);
+
+  useEffect(() => {
+    console.log(repos);
+  }, [repos]);
+  return (
+    <>
+      <ul>
+        {repos.map(repo => (
+          <li key={repo.name}>
+            <a href={repo.html_url}>{repo.name}</a> - {repo.description} (
+            {repo.language}) <Star size={16} /> {repo.stargazers_count}{" "}
+            <GitFork size={16} />
+            {repo.forks}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default GithubRepo;
